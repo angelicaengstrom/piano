c7d2bb43459ed9a4c6b025926bc0d731de7dc01d
angen <angelica.princess@hotmail.com>
2024-03-17 13:58:33 +0100

    Add doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "./src/mainwindow.h"
 
 /**
+ * @brief Main function for the piano.
+ *
  * A GUI-based project where you can play a virtual piano.
  * Incorporation of asynchronous functions (multithreading) along with dealing with different audio libraries.
  * Function that records the tunes played on the piano (file-handling).
