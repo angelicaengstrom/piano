4f8878609b01feaee68d14a637700def370a4107
angen <angelica.princess@hotmail.com>
2024-04-02 12:07:47 +0200

    Add base piano #9

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,27 +10,8 @@ MainWindow::MainWindow(QWidget *parent) :
     QWidget(parent), ui(new Ui::MainWindow), piano(std::make_unique<Piano>()) {
   ui->setupUi(this);
 
-  main_layout = std::make_unique<QGridLayout>();
-
-  time_label = std::make_unique<QLabel>();
-  time_label->setText(QString("s"));
-
-  record_button = std::make_unique<QPushButton>();
-
-  /*auto central_widget = findChild<QWidget*>("centralwidget");
+  auto central_widget = findChild<QWidget*>("centralwidget");
   auto grid_layout = central_widget->findChild<QGridLayout*>("gridlayout");
-  grid_layout->addWidget(piano.release(), 3, 0);
-*/
-
-  //grid_layout->addWidget(record_button.release(), 1, 0, 1, 2);
-  /*
-  main_layout->addWidget(header_label.release(), 0, 0);
-  main_layout->addWidget(record_button.release(), 1, 0, 1, 2);
-  main_layout->addWidget(time_label.release(), 1, 2);
-  main_layout->addWidget(piano.release(), 2, 6, 4, 6);
-
-  setLayout(main_layout.release());
-   */
 }
 
 MainWindow::~MainWindow() {
